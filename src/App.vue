ef10a5b1c9c6870736837fd5090d41ddc5214c54
toto <toto6321@qq.com>
2018-12-27 03:09:30 -0500

    FEATURE: add scss

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import Layout from "./components/Layout";
+import Layout from "./components/Layout/Layout";
 
 export default {
   name: 'app',
---
+++ src/components/Layout/Layout.vue
@@ -0,0 +1,35 @@
+<template>
+    <el-container class="layout">
+        <el-header><h3>On my way to ...</h3></el-header>
+        <el-container>
+            <el-aside>
+                <el-card>
+                    <img alt="icon" src="">
+                    <span></span>
+                    <strong>name</strong>
+                </el-card>
+                <el-card>
+                    <el-button>Upload</el-button>
+                </el-card>
+            </el-aside>
+            <el-main>
+                <div id="gmap">
+
+                </div>
+            </el-main>
+        </el-container>
+        <el-footer>
+            <strong>xingzhe</strong>
+        </el-footer>
+    </el-container>
+</template>
+
+<script>
+    export default {
+        name: "Layout"
+    }
+</script>
+
+<style scoped lang="sass">
+    @import index
+</style>
