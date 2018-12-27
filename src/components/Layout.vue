0053d33c197d4fb3a4a85e245c47f85109b0a3fb
toto <toto6321@qq.com>
2018-12-27 01:04:54 -0500

    FEATURE: add Element-UI

---
+++ src/components/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+    <el-container>
+        <el-header>Header</el-header>
+        <el-container>
+            <el-aside>Aside</el-aside>
+            <el-main>Main</el-main>
+        </el-container>
+        <el-footer>Footer</el-footer>
+    </el-container>
+</template>
+
+<script>
+    export default {
+        name: "Layout"
+    }
+</script>
+
+<style scoped>
+
+</style>
