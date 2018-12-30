b9b67cb5f9ae777f5b7a46315ffc9ea7975e9f54
toto <toto6321@qq.com>
2018-12-30 02:10:00 -0500

    FEATURE: set the width and height of the map

--- src/components/Layout/Layout.vue
+++ src/components/Layout/Layout.vue
@@ -17,7 +17,6 @@
             :center="{lat:10, lng:10}"
             :zoom="7"
             map-type-id="terrain"
-            style="width: 500px; height: 300px"
         >
           <GmapMarker
               :clickable="true"
